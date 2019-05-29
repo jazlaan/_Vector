a60d86c4cfb5ad56355bbdb1ebf756a4bf5db5b9
Jazlaan <hi@jazlaan.com>
2019-05-30 00:07:25 +1000

    Setup Canvas ui elements

--- src/com/vectors/App.java
+++
@@ -1,9 +0,0 @@
-package com.vectors;
-
-import javax.swing.*;
-
-public class App {
-    //initial push.
-    private JPanel mainPanel;
-    private JButton fileJmenu;
-}
---
+++ src/com/vectors/main.java
@@ -0,0 +1,8 @@
+package com.vectors;
+
+
+public class main {
+    public static void main (String[] args){
+        userInterface.main(args);
+    }
+}
